8c7f9c29f14991db42670d1e4163c16728245859
Jörg Steffen <steffen@dfki.de>
2017-06-07 15:49:13 +0200

    Add configuration management

---
+++ src/main/java/de/dfki/lt/mdparser/config/ConfigKeys.java
@@ -0,0 +1,20 @@
+package de.dfki.lt.mdparser.config;
+
+/**
+ * Defines all configuration keys as constants.
+ *
+ * @author Jörg Steffen, DFKI
+ */
+public final class ConfigKeys {
+
+  public static final String MODEL_BUILD_FOLDER = "model.build.folder";
+  public static final String MODEL_OUTPUT_FOLDER = "model.output.folder";
+  public static final String TRAINING_THREADS = "training.threads";
+  public static final String PARSING_THREADS = "parsing.threads";
+
+
+  private ConfigKeys() {
+
+    // private constructor to enforce noninstantiability
+  }
+}
